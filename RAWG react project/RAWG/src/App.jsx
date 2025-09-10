73740db312ab3f8afcae53b2a845a2889a054dd6
Nabhag2507 <nabhagsorathiya81@gmail.com>
2025-09-10 22:02:17 +0530

    pushed for aniruddh

--- RAWG react project/RAWG/src/App.jsx
+++ RAWG react project/RAWG/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
+import Layout from './Layout'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello world</h1>
+      <Layout/>
     </>
   )
 }
